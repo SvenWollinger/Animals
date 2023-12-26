1926eb1972a4001ba4fb5f52bd055ff0852a41bb
SvenWollinger <accounts@wollinger.email>
2023-12-26 11:15:54 +0100

    showing vector2 some lovel <3

--- src/main/kotlin/io/wollinger/animals/math/Vector2.kt
+++ src/main/kotlin/io/wollinger/animals/math/Vector2.kt
@@ -7,34 +7,36 @@ import kotlin.math.sqrt
 
 @Serializable
 class Vector2(var x: Double = 0.0, var y: Double = 0.0) {
-    fun distance(other: Vector2) = sqrt((other.y - this.y).pow(2) + (other.x - this.x).pow(2))
-    fun set(x: Double = this.x, y: Double = this.y) {
-        this.x = x
-        this.y = y
-    }
+    constructor(x: Number, y: Number): this(x.toDouble(), y.toDouble())
 
-    operator fun plus(other: Vector2) = Vector2(x + other.x, y + other.y)
-    operator fun minus(other: Vector2) = Vector2(x - other.x, y - other.y)
+    operator fun plus(vector: Vector2)  = Vector2(x + vector.x, y + vector.y)
+    operator fun minus(vector: Vector2) = Vector2(x - vector.x, y - vector.y)
+    operator fun div(n: Int)            = Vector2(x / n, y / n)
+    operator fun times(n: Int)          = Vector2(x * n, y * n)
 
-    operator fun div(n: Int): Vector2 {
-        return Vector2(x / n, y / n)
-    }
-
-    override fun equals(other: Any?): Boolean {
-        return other is Vector2 && x == other.x && y == other.y
-    }
-
-    fun limit(maxX: Double, maxY: Double) {
+    fun limit(maxX: Number, maxY: Number) = limit(maxX.toDouble(), maxY.toDouble())
+    private fun limit(maxX: Double, maxY: Double) {
         if(x > maxX) x = maxX
         if(y > maxY) y = maxY
     }
-    fun min(minX: Double, minY: Double) {
+
+    fun min(minX: Number, minY: Number) = min(minX.toDouble(), minY.toDouble())
+    private fun min(minX: Double, minY: Double) {
         if(x < minX) x = minX
         if(y < minY) y = minY
-
     }
-    override fun toString() = "Vector2(x=${x.toFixed(2)}, y=${y.toFixed(2)})"
+
+    fun dst(other: Vector2) = sqrt((other.y - this.y).pow(2) + (other.x - this.x).pow(2))
+
+    fun set(x: Number = this.x, y: Number = this.y) {
+        this.x = x.toDouble()
+        this.y = y.toDouble()
+    }
+
     override fun hashCode() = 31 * x.hashCode() + y.hashCode()
+    override fun equals(other: Any?) = other is Vector2 && x == other.x && y == other.y
+
+    override fun toString() = "Vector2(x=${x.toFixed(2)}, y=${y.toFixed(2)})"
 
     companion object {
         fun fromDynamic(vector2: dynamic) = Vector2(vector2.x as Double, vector2.y as Double)
